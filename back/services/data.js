721e4c2bfbe4b67930b7971f0a77b51e6cd5fbe1
Phylosius <hei.phylosius@gmail.com>
2024-08-20 14:45:51 +0300

    refactor: moved data.json to ./back/public/data

---
+++ back/services/data.js
@@ -0,0 +1,53 @@
+import fs from 'node:fs/promises';
+
+/**
+ * Lit un fichier json selon le chemin donnée
+ *
+ * @param path {String} Chemin vers le fichier à lire
+ * @return {
+ *  status: String,
+ *  data: String
+ * }
+ * */
+async function readFile(path) {
+    try {
+        const data = await fs.readFile(path, { encoding: 'utf8' });
+        return {
+            status: "OK",
+            data: JSON.parse(data),
+        };
+    } catch (err) {
+        return {
+            status: "ERROR",
+            error: err,
+        };
+    }
+}
+
+/**
+ * Ecrit un fichier json selon le chemin donnée
+ *
+ * @param path {String} Chemin vers le fichier à lire
+ * @param data {{}} Donnée à écrire dans le fichier JSON
+ * @return {
+ *  status: String,
+ *  error: String
+ * }
+ * */
+async function writeFile(path, data) {
+    try {
+        await fs.writeFile(path, JSON.stringify(data), {
+            encoding: 'utf8',
+        });
+        return {
+            status: "OK",
+        };
+    } catch (err) {
+        return {
+            status: "ERROR",
+            error: err,
+        };
+    }
+}
+
+export { readFile, writeFile };
