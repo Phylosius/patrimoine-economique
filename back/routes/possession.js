9d30abb459bdba36cf26c5e07abb0162076b0c04
Phylosius <hei.phylosius@gmail.com>
2024-08-20 18:13:51 +0300

    feat: added post endpoint /possession

--- back/routes/possession.js
+++ back/routes/possession.js
@@ -1,10 +1,22 @@
 import express from 'express';
-import {getPossessionList} from "../services/possession.js";
+import {createPossession, getPossessionList} from "../services/possession.js";
+import Possession from "../../models/possessions/Possession.js";
 const router = express.Router();
 
 router.get('/', (req, res) => {
     getPossessionList().then((possessionList) => {
         res.send(possessionList);
+    })
+})
+
+router.post('/', (req, res) => {
+    createPossession(
+        req.body.libelle,
+        req.body.valeur,
+        req.body.dateDebut,
+        req.body.taux
+    ).then(()=>{
+        res.sendStatus(200);
     })
 })
 
