f2f686d2aef6d49a19640eb98094c1c5712e6f70
Phylosius <hei.phylosius@gmail.com>
2024-08-23 16:11:34 +0300

    feat: added req.params check for POST /possession

--- back/routes/possession.js
+++ back/routes/possession.js
@@ -14,14 +14,15 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    createPossession(
-        req.body.libelle,
-        req.body.valeur,
-        req.body.dateDebut,
-        req.body.taux
-    ).then(()=>{
-        res.sendStatus(200);
-    })
+    if (!req.params.model) {
+        createPossession(
+            req.body.libelle,
+            req.body.valeur,
+            req.body.dateDebut,
+            req.body.taux
+        ).then(()=>{res.sendStatus(200);})
+    }
+
 })
 
 router.post('/:libelle', (req, res) => {
