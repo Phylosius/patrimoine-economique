c38cf0dc03e9ae7472e9fe5fd959e9ba3cc2757a
Phylosius <hei.phylosius@gmail.com>
2024-08-23 15:55:46 +0300

    feat: added Possession's childs support for POST /possession/:libelle

--- back/routes/possession.js
+++ back/routes/possession.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import {createPossession, getPossessionList, updatePossession} from "../services/possession.js";
 import Possession from "../../models/possessions/Possession.js";
+import Personne from "../../models/Personne.js";
+import Flux from "../../models/possessions/Flux.js";
+import Argent from "../../models/possessions/Argent.js";
+import BienMateriel from "../../models/possessions/BienMateriel.js";
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -27,6 +31,38 @@ router.post('/:libelle', (req, res) => {
         if (!req.params.model) {
             updatePossession(cibleLibelle, new Possession(req.body.possesseur, req.body.libelle, req.body.valeur, req.body.dateDebut, req.body.dateFin, req.body.tauxAmortissement))
                 .then(() => {res.sendStatus(200);})
+        } else {
+            if (req.params.model === "Flux") {
+                updatePossession(cibleLibelle, new Flux(
+                    new Personne(req.body.possesseur.name),
+                    req.body.libelle,
+                    req.body.valeur,
+                    req.body.dateDebut,
+                    req.body.dateFin,
+                    req.body.tauxAmortissement,
+                    req.body.jour
+                )).then(r => {res.sendStatus(200);})
+            } else if (req.params.model === "Argent") {
+                updatePossession(cibleLibelle, new Argent(
+                    new Personne(req.body.possesseur.name),
+                    req.body.libelle,
+                    req.body.valeur,
+                    req.body.dateDebut,
+                    req.body.dateFin,
+                    req.body.tauxAmortissement,
+                    req.body.type
+                )).then(r => {res.sendStatus(200);})
+
+            } else if (req.params.model === "BienMateriel") {
+                updatePossession(cibleLibelle, new BienMateriel(
+                    new Personne(req.body.possesseur.name),
+                    req.body.libelle,
+                    req.body.valeur,
+                    req.body.dateDebut,
+                    req.body.dateFin,
+                    req.body.tauxAmortissement
+                )).then(r => {res.sendStatus(200);})
+            }
         }
 
     } else {
