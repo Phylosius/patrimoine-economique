614418ec878d158ad233184be8cbfc9d8c9b2b40
Phylosius <hei.phylosius@gmail.com>
2024-08-26 15:32:25 +0300

    feat: created GET /patrimoine/range

--- backend/routes/patrimoine.js
+++ backend/routes/patrimoine.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import {getValeurPatrimoine} from "../controllers/patrimoine.js";
+import {getPatrimoineRangeByMonth, getValeurPatrimoine} from "../controllers/patrimoine.js";
 const router = express.Router();
 
 router.get('/:date',(req,res)=>{
     getValeurPatrimoine(req, res);
+})
+
+router.get('/range', (req,res)=>{
+    getPatrimoineRangeByMonth(req, res);
 })
 
 export default router;
