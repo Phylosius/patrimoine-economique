5342ab38a3fe989ae4e07dab9facb1ba3f61d3dc
Phylosius <hei.phylosius@gmail.com>
2024-08-26 16:06:04 +0300

    fix: GET /patrimoine/range not work

--- backend/routes/patrimoine.js
+++ backend/routes/patrimoine.js
@@ -2,12 +2,13 @@ import express from 'express';
 import {getPatrimoineRangeByMonth, getValeurPatrimoine} from "../controllers/patrimoine.js";
 const router = express.Router();
 
+router.get('/range', (req,res)=>{
+    getPatrimoineRangeByMonth(req, res);
+})
+
 router.get('/:date',(req,res)=>{
     getValeurPatrimoine(req, res);
 })
 
-router.get('/range', (req,res)=>{
-    getPatrimoineRangeByMonth(req, res);
-})
 
 export default router;
