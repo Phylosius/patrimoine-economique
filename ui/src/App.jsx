ee5e07d69062ef46ce6d77a2c3292394f801562e
Phylosius <hei.phylosius@gmail.com>
2024-08-26 16:53:02 +0300

    feat: created Route for App.jsx

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Header from './components/Header';
-// import Patrimoine from './pages/Patrimoine';
-// import ListPossession from './pages/ListPossession';
-// import CreatePossession from './pages/CreatePossession';
-// import UpdatePossession from './pages/UpdatePossession';
+import Header from './components/Header';
+import Patrimoine from './pages/Patrimoine';
+import ListPossession from './pages/ListPossession';
+import CreatePossession from './pages/CreatePossession';
+import UpdatePossession from './pages/UpdatePossession';
 
 function App() {
   return (
       <Router>
-        {/*<Header />*/}
+        <Header />
         <Routes>
           <Route path="/patrimoine" element={<Patrimoine />} />
           <Route path="/possession" element={<ListPossession />} />
