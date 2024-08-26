ed1b2aa52f88231aa2aa8e8aeabfe00689b381ab
Phylosius <hei.phylosius@gmail.com>
2024-08-26 16:52:40 +0300

    feat: created Route for App.jsx

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Header from './components/Header';
+// import Patrimoine from './pages/Patrimoine';
+// import ListPossession from './pages/ListPossession';
+// import CreatePossession from './pages/CreatePossession';
+// import UpdatePossession from './pages/UpdatePossession';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-
-    </>
-  )
+      <Router>
+        {/*<Header />*/}
+        <Routes>
+          <Route path="/patrimoine" element={<Patrimoine />} />
+          <Route path="/possession" element={<ListPossession />} />
+          <Route path="/possession/create" element={<CreatePossession />} />
+          <Route path="/possession/:libelle/update" element={<UpdatePossession />} />
+        </Routes>
+      </Router>
+  );
 }
 
-export default App
+export default App;
+
