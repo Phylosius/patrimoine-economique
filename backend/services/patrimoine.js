5c8b12ea798b1f9951dc01e5ac42295524bc0835
Phylosius <hei.phylosius@gmail.com>
2024-08-26 15:14:20 +0300

    feat: created getDateRange() in services/patrimoine

--- backend/services/patrimoine.js
+++ backend/services/patrimoine.js
@@ -7,3 +7,30 @@ export async function getPatrimoine() {
     return new Patrimoine(possessions[0].possesseur, possessions);
 }
 
+export function getDateRange(start, end, by="month", dayForMiddle = null) {
+    let dates = [];
+    if (by === "month") {
+        const monthBetween = getMonthBetween(start, end);
+
+        dates.push(start);
+        for (let i = 1; i <= monthBetween; i++) {
+            const date = new Date((new Date(start)).setMonth(start.getMonth() + i));
+            dayForMiddle ? date.setDate(dayForMiddle): null;
+            dates.push(date);
+        }
+        dates.push(end);
+    }
+
+    return dates;
+}
+
+export function getMonthBetween(start, end) {
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+
+    return endDate.getMonth() - startDate.getMonth();
+}
+
+export function getMonthBetweenDate(start, end) {
+    return getMonthBetween(start.toISOString(), end.toISOString());
+}
