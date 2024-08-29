67be58924c8d03d6de5512ce5c0bd2a03a6d34c1
Phylosius <hei.phylosius@gmail.com>
2024-08-29 15:38:27 +0300

    fix: POST /possession 404

--- ui/src/pages/CreatePossession.jsx
+++ ui/src/pages/CreatePossession.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import axios from 'axios';
+import axios from '../axiosConfig.js';
 import { useNavigate } from 'react-router-dom';
 import { Container, Form, Button } from 'react-bootstrap';
 
