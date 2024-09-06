8e2034bd50bf1abe960cf943c0b336b2a9030cd3
Phylosius <hei.phylosius@gmail.com>
2024-09-06 14:22:02 +0300

    feat: added backend repo

---
+++ backend/draft.js
@@ -0,0 +1,8 @@
+import {getPossession} from "./services/possession.js";
+import {log} from "debug";
+
+// getPossession("MacBook Pro").then(p=>{
+//     log(p.getValeur(new Date("2023-01-01")))
+// })
+
+console.log(-1 || 1)
