be87932841c621fcedc9116b0ef728df9e6e2194
Phylosius <hei.phylosius@gmail.com>
2024-09-06 15:51:31 +0300

    updated: ui

--- backend/draft.js
+++
@@ -1,8 +0,0 @@
-import {getPossession} from "./services/possession.js";
-import {log} from "debug";
-
-// getPossession("MacBook Pro").then(p=>{
-//     log(p.getValeur(new Date("2023-01-01")))
-// })
-
-console.log(-1 || 1)
